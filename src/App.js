f851915fbda572f232d795b57a5330724789b18e
Manoj Kumar C <manojkc9845@gmail.com>
2022-05-28 00:09:55 +0530

    modified

--- src/App.js
+++ src/App.js
@@ -1,19 +1,38 @@
+import React,{useEffect,useState} from "react";
 import "./App.css";
 import SignUp from "./components/Auth/SignUp";
 import Login from "./components/Auth/Login";
-import ProviderRegister from "./components/Providers/ProviderRegister";
-import { Route, Routes, Navigate } from "react-router-dom";
+import { Route, Routes, Navigate, BrowserRouter } from "react-router-dom";
+import MapViewer from "./components/Map/MapViewer";
+
 function App() {
-    return (
-        <div>
-            <ProviderRegister />
-            {/* <Routes>
-                <Route path="/login" element={<Login />} />
-                <Route path="/Sign-Up" element={<SignUp />} />
-                <Route path="/" element={<Navigate to="/login" />} />
-            </Routes> */}
-        </div>
-    );
+
+    const [lat,setLat] = useState()
+    const [lon,setLon] = useState()
+
+     useEffect(()=>{
+        navigator.geolocation.getCurrentPosition(function(position) {
+            setLat(position.coords.latitude)
+            console.log("Latitude is :", position.coords.latitude);
+            setLon(position.coords.longitude)
+            console.log("Longitude is :", position.coords.longitude);
+          });
+     },[])
+       
+      
+
+  return (
+    <BrowserRouter>
+      <div>
+        <Routes>
+          <Route path="/login" element={<Login/>} />
+          <Route path="/Sign-Up" element={<SignUp />} />
+          <Route exact path="/" element={<Navigate to="/login" />} />
+          <Route path="/map" element={<MapViewer lat={lat} lon={lon}/>} />
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
 }
 
 export default App;
