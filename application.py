e837fa538d97c27f46a1afbaac5446bf13d77055
Gowtham M <55031924+Gowtham-M1729@users.noreply.github.com>
2022-05-27 19:27:56 +0530

    Update application.py

--- application.py
+++ application.py
@@ -17,6 +17,61 @@ app.secret_key = b'\xcc^\x91\xea\x17-\xd0W\x03\xa7\xf8J0\xac8\xc5'
 url = "mongodb+srv://user1234:59dLdTzaKaqimTt@user.vl67u.mongodb.net/user_login_system?retryWrites=true&w=majority"
 client = pymongo.MongoClient(url)
 db = client.customer_details
+
+
+class provider:
+    def start_session(self, provider):
+        print(provider)
+        del provider['password']
+        session['logged_in'] = True
+        session['provider'] = provider
+        return jsonify(provider), 200
+
+    def apply(self, name, email, password,mob_no,lat,long):
+        # print(request.form)
+        print("dfg")
+        # Create the provider object
+        provider = {
+            "_id": uuid.uuid4().hex,
+            "p_name": name,
+            "p_email": email,
+            "mobile_no": mob_no,
+            "latitude": lat,
+            "longitude": long,
+            "password": password
+        }
+        print("dfg")
+        # Encrypt the password
+        provider['password'] = pbkdf2_sha256.encrypt(provider['password'])
+
+        # Check for existing email address
+        if db.provide.find_one({"p_email": provider['p_email']}):
+            return jsonify({"error": "Email address already in use"}), 400
+        print("dfg")
+        if db.provide.insert_one(provider):
+            self.start_session(provider)
+            return jsonify({"message":"Succesfully signed up"})
+
+        return jsonify({"error": "Signup failed"}), 400
+
+    def login(self, email, password):
+        print("Hello", email, password)
+
+        provider = db.provide.find_one({
+            "p_email": email
+        })
+        print(provider)
+        # print(pbkdf2_sha256.verify(password, provider['password']))
+        if provider and pbkdf2_sha256.verify(password, provider['password']):
+            print("Login sucessful")
+            self.start_session(provider)
+            return jsonify({"message":"Sucessfully Logged in"})
+
+        return jsonify({"error": "Invalid login credentials"}), 401
+
+
+
+
 class User:
     def start_session(self, user):
         print(user)
@@ -97,6 +152,25 @@ def login():
         print(body)
         return  User().login(body['email'], body['password'])
 
+@app.route('/provider/apply', methods=['POST'])
+def apply():
+    body = request.json
+    print(body)
+    print(body['email'], body['password'])
+    return provider().apply(body['name'], body['email'], body['password'],body['mob_no'],body['lat'],body['long'])
+
+
+@app.route('/provider/signout')
+def signout():
+    return provider().signout()
+
+
+@app.route('/provider/login', methods=['POST'])
+def login():
+    if request.method == 'POST':
+        body = request.json
+        print(body)
+        return  provider().login(body['email'], body['password'])
 
 if __name__ == "__main__":
     # TODO Valid return statements for all routes
