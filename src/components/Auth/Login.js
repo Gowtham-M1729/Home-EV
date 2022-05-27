d60784a4671da2e3416b1757cd2684643d957d85
Manoj Kumar C <manojkc9845@gmail.com>
2022-05-27 19:25:52 +0530

    updates sign-in

--- src/components/Auth/Login.js
+++ src/components/Auth/Login.js
@@ -1,9 +1,83 @@
-import React from 'react'
+import useInput from "../hooks/use-input";
+import "./SignUp.css";
+import { Link } from "react-router-dom";
 
 const Login = () => {
-  return (
-    <div>Login</div>
-  )
-}
+    const {
+        value: userName,
+        valueIsValid: userNameIsValid,
+        hasError: userHasError,
+        inputHandler: userInputHandler,
+        onBlurHandler: usertBlurHandler,
+        reset: userReset,
+    } = useInput((value) => value.includes("@"));
 
-export default Login
+    const {
+        value: password,
+        valueIsValid: passIsValid,
+        hasError: passHasError,
+        inputHandler: passInputHandler,
+        onBlurHandler: passBlurHandler,
+        reset: passReset,
+    } = useInput((value) => value.trim() !== "");
+
+    let isFormValid = false;
+    if (userNameIsValid && passIsValid) isFormValid = true;
+
+    const onSubmitHandler = (event) => {
+        event.preventDefault();
+
+        if (!userNameIsValid && !passIsValid) return;
+
+        console.log(userName, password);
+        userReset();
+        passReset();
+    };
+
+    const userClass = userHasError ? "form-control invalid" : "form-control";
+    const passClass = passHasError ? "form-control invalid" : "form-control";
+
+    return (
+        <form onSubmit={onSubmitHandler}>
+            <div className="signup-box">
+                <h3>Login</h3>
+                <div className="control-group">
+                    <div className={userClass}>
+                        <label htmlFor="name">userName</label>
+                        <input
+                            type="text"
+                            id="name"
+                            onChange={userInputHandler}
+                            onBlur={usertBlurHandler}
+                            value={userName}
+                        />
+                        {userHasError && (
+                            <p className="error-text">Enter valid Email</p>
+                        )}
+                    </div>
+                    <div className={passClass}>
+                        <label htmlFor="name">Password</label>
+                        <input
+                            type="password"
+                            id="name"
+                            onChange={passInputHandler}
+                            onBlur={passBlurHandler}
+                            value={password}
+                        />
+                        {passHasError && (
+                            <p className="error-text">Enter valid Password</p>
+                        )}
+                    </div>
+                    <div className="form-actions">
+                        <Link to="/Sign-Up" className="link">
+                            SignUp Instead
+                        </Link>
+                        <button disabled={!isFormValid}>Submit</button>
+                    </div>
+                </div>
+            </div>
+        </form>
+    );
+};
+
+export default Login;
--- src/components/Auth/SignUp.js
+++ src/components/Auth/SignUp.js
@@ -1,79 +1,119 @@
+import React, { useState } from "react";
 import useInput from "../hooks/use-input";
 import "./SignUp.css";
+import { Link } from "react-router-dom";
 
-const BasicForm = (props) => {
-  const {
-    value: userName,
-    valueIsValid: userNameIsValid,
-    hasError: userHasError,
-    inputHandler: userInputHandler,
-    onBlurHandler: usertBlurHandler,
-    reset: userReset,
-  } = useInput((value) => value.includes("@"));
+const SignUp = () => {
+    const [error, setError] = useState(false);
 
-  const {
-    value: password,
-    valueIsValid: passIsValid,
-    hasError: passHasError,
-    inputHandler: passInputHandler,
-    onBlurHandler: passBlurHandler,
-    reset: passReset,
-  } = useInput((value) => value.trim() !== "");
+    const {
+        value: userName,
+        valueIsValid: userNameIsValid,
+        hasError: userHasError,
+        inputHandler: userInputHandler,
+        onBlurHandler: userBlurHandler,
+        reset: userReset,
+    } = useInput((value) => value.includes("@"));
 
-  let isFormValid = false;
-  if (userNameIsValid && passIsValid) isFormValid = true;
+    const {
+        value: password,
+        valueIsValid: passIsValid,
+        hasError: passHasError,
+        inputHandler: passInputHandler,
+        onBlurHandler: passBlurHandler,
+        reset: passReset,
+    } = useInput((value) => value.trim() !== "");
 
-  const onSubmitHandler = (event) => {
-    event.preventDefault();
+    const {
+        value: confPassword,
+        valueIsValid: confPassIsValid,
+        hasError: confPassHasError,
+        inputHandler: confPassInputHandler,
+        onBlurHandler: confPassBlurHandler,
+        reset: confPassReset,
+    } = useInput((value) => value.trim() !== "");
 
-    if (!userNameIsValid && !passIsValid) return;
+    let isFormValid = false;
+    if (userNameIsValid && passIsValid && confPassIsValid) isFormValid = true;
 
-    console.log(userName, password);
-    userReset();
-    passReset();
-  };
+    const onSubmitHandler = (event) => {
+        event.preventDefault();
 
-  const userClass = userHasError ? "form-control invalid" : "form-control";
-  const passClass = passHasError ? "form-control invalid" : "form-control";
+        if (!userNameIsValid && !passIsValid && !confPassIsValid) return;
 
-  return (
-    <form onSubmit={onSubmitHandler}>
-      <div className="signup-box">
-        <h3>Sign Up!</h3>
-        <div className="control-group">
-          <div className={userClass}>
-            <label htmlFor="name">userName</label>
-            <input
-              type="text"
-              id="name"
-              onChange={userInputHandler}
-              onBlur={usertBlurHandler}
-              value={userName}
-            />
-            {userHasError && (
-              <p className="error-text">Enter valid Email</p>
-            )}
-          </div>
-          <div className={passClass}>
-            <label htmlFor="name">Password</label>
-            <input
-              type="password"
-              id="name"
-              onChange={passInputHandler}
-              onBlur={passBlurHandler}
-              value={password}
-            />
-            {passHasError && (
-              <p className="error-text">Enter valid Password</p>
-            )}
-          </div>
-          <div className="form-actions">
-            <button disabled={!isFormValid}>Submit</button>
-          </div>
-        </div>
-      </div>
-    </form>
-  );
+        if (password !== confPassword) setError(true);
+
+        console.log(userName, password);
+        userReset();
+        passReset();
+        confPassReset();
+    };
+
+    const userClass = userHasError ? "form-control invalid" : "form-control";
+    const passClass = passHasError ? "form-control invalid" : "form-control";
+    const confPassClass = confPassHasError
+        ? "form-control invalid"
+        : "form-control";
+
+    return (
+        <form onSubmit={onSubmitHandler}>
+            <div className="signup-box">
+                <h3>Sign Up!</h3>
+                <div className="control-group">
+                    <div className={userClass}>
+                        <label htmlFor="name">userName</label>
+                        <input
+                            type="text"
+                            id="name"
+                            onChange={userInputHandler}
+                            onBlur={userBlurHandler}
+                            value={userName}
+                        />
+                        {userHasError && (
+                            <p className="error-text">Enter valid Email</p>
+                        )}
+                    </div>
+                    <div className={passClass}>
+                        <label htmlFor="name">Password</label>
+                        <input
+                            type="password"
+                            id="password"
+                            onChange={passInputHandler}
+                            onBlur={passBlurHandler}
+                            value={password}
+                        />
+                        {passHasError && (
+                            <p className="error-text">Enter valid Password</p>
+                        )}
+                    </div>
+                    <div className={confPassClass}>
+                        <label htmlFor="name">confirm Password</label>
+                        <input
+                            type="password"
+                            id="confirm password"
+                            onChange={confPassInputHandler}
+                            onBlur={confPassBlurHandler}
+                            value={confPassword}
+                        />
+                        {confPassHasError && (
+                            <p className="error-text">Enter valid Password</p>
+                        )}
+                    </div>
+                    {error && (
+                        <p style={{ color: "red" }}>
+                            *check the password again
+                        </p>
+                    )}
+                    <div className="form-actions">
+                        <Link Navigate to="/login" className="link">
+                            Login In
+                        </Link>
+                        <button disabled={!isFormValid}>Submit</button>
+                    </div>
+                </div>
+            </div>
+        </form>
+    );
 };
 
-export default BasicForm;
+export default SignUp;
